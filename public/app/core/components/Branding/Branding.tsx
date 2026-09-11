9da384ace85b383918ab61937f5430cba3005717
mtrumble <matthew.trumble@nhs.net>
2026-09-11 15:08:11 +0100

    Change branding titles

--- public/app/core/components/Branding/Branding.tsx
+++ public/app/core/components/Branding/Branding.tsx
@@ -147,8 +147,8 @@ export class Branding {
   static LoginBackground = LoginBackground;
   static MenuLogo = MenuLogo;
   static LoginBoxBackground = LoginBoxBackground;
-  static AppTitle = 'Grafana';
-  static LoginTitle = 'Welcome to Grafana';
+  static AppTitle = 'PROJECT CMS';
+  static LoginTitle = 'Welcome to PROJECT CMS';
   static HideEdition = false;
   static GetLoginSubTitle = (): null | string => {
     return null;
